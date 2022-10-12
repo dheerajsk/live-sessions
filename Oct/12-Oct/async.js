b335705046dea4f14e19bdcda88174371dfdbe05
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-10-12 22:41:11 +0530

    Async

---
+++ Oct/12-Oct/async.js
@@ -0,0 +1,12 @@
+
+console.log("Start of Program");
+
+var x = 20;
+var y = 10;
+
+// executes a task after given time.
+setTimeout(()=>{
+    console.log(x+y);
+},0);
+
+console.log("End of Program");
---
+++ Oct/12-Oct/sync.js
@@ -0,0 +1,8 @@
+
+console.log("Start of Program");
+var x = 10;
+var y = 20;
+
+console.log(x+y);
+
+console.log("End of Program");
