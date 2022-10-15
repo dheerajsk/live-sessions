ea94b5d6a41b0a129be85ba7e619925f81764cbc
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-10-15 12:27:57 +0530

    Async Await.

--- Oct/14-Oct/promise/async-await.js
+++ Oct/14-Oct/promise/async-await.js
@@ -6,13 +6,14 @@ async function placeOrder(){
 
     // wait for this to finish before executing next statements.
     try{
-        const result = await verifyCardModule.verify(2021);
-        console.log(result);    
+        const result = await verifyCardModule.verify(2022);
+        console.log(result);   
+        deductAmount(); 
+        // throw new Error("Error after promise is resolved");
     }catch(err){
         console.log(err);
         return;
     }
-    deductAmount();
 }
 
 
