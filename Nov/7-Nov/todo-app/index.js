7e040afab71b025c37dbd3bbe32d49d7ed9667c9
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-11-11 20:57:13 +0530

    To do

--- Nov/7-Nov/todo-app/index.js
+++ Nov/7-Nov/todo-app/index.js
@@ -18,7 +18,7 @@ function handleToDoFormSubmit(){
 function createTodoListItem(text){
     todos.push(text);
 
-    const li="<li><label id=todo"+todos.length+" style='width:80%'>"+text+"</label><button id=editBtn"+todos.length+" onclick='handleEditClick(event)' class='btn btn-primary'>Edit</button>&nbsp;&nbsp;&nbsp;<button class='btn btn-danger'>Delete</button></li>"
+    const li="<li id=listItem"+todos.length+"><label id=todo"+todos.length+" style='width:80%'>"+text+"</label><button id=editBtn"+todos.length+" onclick='handleEditClick(event)' class='btn btn-primary'>Edit</button>&nbsp;&nbsp;&nbsp;<button class='btn btn-danger'>Delete</button></li>"
     // Find the ul where we need to add created li.
     const ul = document.getElementById("todoList");
 
@@ -28,12 +28,32 @@ function createTodoListItem(text){
 }
 
 function handleEditClick(event){
-    console.log(event.target.id);
-    const labelID=event.target.id.toString().replace("editBtn","todo");
+   
+    // 1. Get ID of button.
+    // 2. using that get id of label.
+    // 3. get label.
+    // 4. create text box.
+    // 5. assign value of textbox to label text.
+    // 6. get li.
+    // 7. add textbox to li.
+
+
+    // event object contains information related to the event.
+    // event.target => points to the element which has raised event.
+    const button = document.getElementById(event.target.id);
+    const labelID=event.target.id.replace("editBtn","todo");
     const label = document.getElementById(labelID);
-    label.style.display="none";
+    
+    
+    button.innerText="Save";
     // label.style.display="none";
 
-    console.log(label);
-    console.log("handling edit click");
+    // Create a text box and add it to li.
+    const txtBox = "<input value='"+label.innerText+"' class='form-control' type='text' id='txtBoxToDoUpdate' />";
+    label.style.display="none";
+    const liID=event.target.id.replace("editBtn","listItem");
+    const li = document.getElementById(liID);
+    li.innerHTML=txtBox+li.innerHTML;
+
+    
 }
